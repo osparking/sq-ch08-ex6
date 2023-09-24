53b4f0fd5132ee688d0a3bce2cf09fc0d5bb77ec
osparking <jbpark03@gmail.com>
2023-09-24 13:27:36 +0900

    initial w/ 3 dependencies

---
+++ src/main/java/ssh_08_ex6/SqCh08Ex6Application.java
@@ -0,0 +1,13 @@
+package ssh_08_ex6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqCh08Ex6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqCh08Ex6Application.class, args);
+	}
+
+}
---
+++ src/test/java/ssh_08_ex6/SqCh08Ex6ApplicationTests.java
@@ -0,0 +1,13 @@
+package ssh_08_ex6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh08Ex6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
