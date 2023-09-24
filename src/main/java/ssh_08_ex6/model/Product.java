fa03fbb93996c2ebe020237013d1e5e295082ec5
osparking <jbpark03@gmail.com>
2023-09-24 13:43:49 +0900

    product model class w/ 2 members

---
+++ src/main/java/ssh_08_ex6/model/Product.java
@@ -0,0 +1,11 @@
+package ssh_08_ex6.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Product {
+	private String name;
+	private double price;
+}
